977b40e805cbca9c08b3539359020fcc05b70a66
Banditolabs <Joshuagosst@gmail.com>
2025-02-11 12:49:32 -0800

    change vite config back

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 8080,
-  },
-  preview: {
-    port: 3000,
-  },
   plugins: [react()],
 })
