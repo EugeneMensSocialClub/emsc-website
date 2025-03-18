52e665d5b3d0d5b9c527f77b9d77a0487181de52
Banditolabs <Joshuagosst@gmail.com>
2025-03-18 13:04:58 -0700

    New styles and structure for carousel

--- src/componenets/homepage/HomeTestimonial.tsx
+++ src/componenets/homepage/HomeTestimonial.tsx
@@ -1,5 +1,5 @@
 import { Carousel } from "@mantine/carousel";
-import { Flex, Center, Title, Text, Paper, Overlay } from "@mantine/core";
+import { Flex, Center, Text, Image, Box, Title } from "@mantine/core";
 import layoverImage from "../../assets/images/Layover.png";
 import craftParty from "../../assets/images/CraftParty.png";
 import smallGroup from "../../assets/images/SmallGroup.png";
@@ -7,106 +7,74 @@ import "../../assets/stylesheets/homepage.css";
 
 export default function HomeTestimonial() {
   return (
-    <Flex className="testimonialContainer" direction="column">
-      <Center className="tesmonialTitleContainer">
-        <Title size="h1" order={2}>
-          {" "}
-          Stories from our Members
-        </Title>
-      </Center>
+    <Flex className="carouselContainer" direction="column">
       <Center>
         <Carousel
-          slideSize={{ base: "50%", sm: "75%" }}
+          slideSize={"75%"}
+          withIndicators
+          withControls={false}
           slideGap={"xl"}
           controlsOffset={"xl"}
           maw={1280}
           loop
         >
           <Carousel.Slide>
-            <Paper
-              shadow="md"
-              p="xl"
-              radius="md"
-              h={500}
-              style={{ backgroundImage: `url(${layoverImage})` }}
-              aria-describedby="Two friends meet at the airport in serendipity."
-              className="card"
-              id="card"
-            >
-              <div></div>
-              <div className="quoteContainer">
-                <Text className="quote" ta="center">
+            <Flex className="carouselSlideContent">
+              <Image
+                className="carouselImage"
+                src={layoverImage}
+                h={"100%"}
+                w={"50%"}
+              />
+              <Box className="carouselTextContainer">
+                <Title className="carouselTitle" order={2}>
+                  TRIVIA NIGHTS
+                </Title>
+                <Text className="carouselText">
                   "If it weren’t for the Eugene men’s social club, I would’ve
                   been alone waiting for my connecting flight back."
                 </Text>
-                <Text>
-                  <b>PETER M.</b>
-                </Text>
-              </div>
-
-              <Overlay
-                gradient="linear-gradient(360deg, rgba(0, 0, 0, .95) 25%, rgba(0, 0, 0, 0) 50%)"
-                opacity={0.85}
-                zIndex={0}
-              />
-            </Paper>
+              </Box>
+            </Flex>
           </Carousel.Slide>
           <Carousel.Slide>
-            <Paper
-              shadow="md"
-              p="xl"
-              radius="md"
-              h={500}
-              style={{ backgroundImage: `url(${craftParty})` }}
-              className="card"
-              aria-describedby="a large group of friends building legos."
-              id="card"
-            >
-              <div></div>
-              <div className="quoteContainer">
-                <Text className="quote" ta="center">
+            <Flex>
+              <Image
+                className="carouselImage"
+                src={craftParty}
+                h={"100%"}
+                w={"50%"}
+              />
+              <Box className="carouselTextContainer">
+                <Title className="carouselTitle" order={2}>
+                  LEGOS ARE FOR ADULTS
+                </Title>
+                <Text className="carouselText">
                   "With EMSC, I've been able to relive my childhood by playing
                   with Legos with my friends."
                 </Text>
-                <Text>
-                  <b>TOM B.</b>
-                </Text>
-              </div>
-              <Overlay
-                gradient="linear-gradient(360deg, rgba(0, 0, 0, .95) 25%, rgba(0, 0, 0, 0) 50%)"
-                opacity={0.85}
-                zIndex={0}
-              />
-            </Paper>
+              </Box>
+            </Flex>
           </Carousel.Slide>
           <Carousel.Slide>
-            <Paper
-              shadow="md"
-              p="xl"
-              radius="md"
-              h={500}
-              style={{ backgroundImage: `url(${smallGroup})` }}
-              className="card"
-              aria-describedby="a small group of friends enjoying a gathering."
-              id="card"
-            >
-              <div></div>
-              <div className="quoteContainer">
-                <Text className="quote" ta="center">
+            <Flex>
+              <Image
+                className="carouselImage"
+                src={smallGroup}
+                h={"100%"}
+                w={"50%"}
+              />
+              <Box className="carouselTextContainer" id="textContainer">
+                <Title className="carouselTitle" order={2}>
+                  SUNDAY MEETUPS
+                </Title>
+                <Text className="carouselText">
                   "After living in Eugene for almost two years, this group gave
                   me a powerful method of making wonderful freinds in a city
                   where it had proven hard to meet people."
                 </Text>
-                <Text>
-                  <b>JOSHUA G.</b>
-                </Text>
-              </div>
-              <Overlay
-                gradient="linear-gradient(360deg, rgba(0, 0, 0, .95) 25%, rgba(0, 0, 0, 0) 50%)"
-                opacity={0.85}
-                zIndex={0}
-              />
-            </Paper>
+              </Box>
+            </Flex>
           </Carousel.Slide>
         </Carousel>
       </Center>
