160c31daa512188dabc8fc3c1d6534179f8442fa
Banditolabs <Joshuagosst@gmail.com>
2025-03-23 14:39:13 -0700

    Rename component names to be more concise and add a new style directory

---
+++ src/componenets/homepage/Testimonial.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { Box, Image, Text } from "@mantine/core";
+import "../../assets/stylesheets/homepage.css";
+
+export default function Testimonial() {
+  return (
+    <Box>
+      <Box>
+        <Image />
+      </Box>
+      <Box>
+        <Image />
+      </Box>
+      <Box>
+        <Image />
+      </Box>
+      <Box>
+        <Text></Text>
+      </Box>
+    </Box>
+  );
+}
