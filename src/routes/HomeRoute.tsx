99bec288bc4fc5ab877053dec3282481b06f3003
Banditolabs <Joshuagosst@gmail.com>
2025-03-23 14:39:13 -0700

    Rename component names to be more concise and add a new style directory

--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -1,9 +1,9 @@
-import { Center, Flex, Box } from "@mantine/core";
-import HomeTestimonial from "../componenets/homepage/HomeTestimonial";
+import { Flex } from "@mantine/core";
+import HomeTestimonial from "../componenets/homepage/Carousel";
 import MobileHomeTestimonial from "../componenets/homepage/mobile/MobileTestimonial";
-import HomeFeaturesSection from "../componenets/homepage/HomeFeaturesSection";
+import HomeFeaturesSection from "../componenets/homepage/Feature";
 import MobileFeaturesSection from "../componenets/homepage/mobile/MobileFeaturesSection";
-import HomeHeroSection from "../componenets/homepage/HomeHeroSection";
+import HomeHeroSection from "../componenets/homepage/Hero";
 import MobileHeroSection from "../componenets/homepage/mobile/MobileHeroSection";
 import "../assets/stylesheets/homepage.css";
 import useIsMobile from "../hooks/useIsMobile";
