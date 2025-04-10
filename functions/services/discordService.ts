7d8833138e04c0c8436fef9c0f85785161140ce8
Banditolabs <Joshuagosst@gmail.com>
2025-04-09 22:18:50 -0700

    Adding fix for firebase config and asset resolution

--- functions/services/discordService.ts
+++ functions/services/discordService.ts
@@ -1,5 +1,5 @@
 import { Client, GatewayIntentBits } from "discord.js";
-import { AppError } from "../errors/errorHandler";
+import { AppError } from "../errors/errorHandler.js";
 
 interface cacheEntry {
   data: any;
